42138fc61ff2a7ae7d5b3dc21868c7469ec2be30
tuffcoding <githubcodingwcopilot@gmail.com>
2026-08-22 02:00:22 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-  {/* load old DOM script after page is interactive */}
-  <Script src="/app.js" strategy="afterInteractive" />
+// pages/_app.js
+import Head from 'next/head'
+import Script from 'next/script'
+// If you converted styles.css into styles/globals.css, uncomment the line below
+// import '../styles/globals.css'
 
-  <Component {...pageProps} />
-</>
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* If you kept styles.css in public/, load it here */}
+        <link rel="stylesheet" href="/styles.css" />
+      </Head>
+
+      {/* load old DOM script after page is interactive */}
+      <Script src="/app.js" strategy="afterInteractive" />
+
+      <Component {...pageProps} />
+    </>
+  )
+}
